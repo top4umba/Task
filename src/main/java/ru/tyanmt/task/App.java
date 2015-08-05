849532ab7873743ee007320311cd6ccf64e11f9a
top4umba <tyanmt@gmail.com>
2015-08-05 19:32:02 +0300

    Init maven project

---
+++ src/main/java/ru/tyanmt/task/App.java
@@ -0,0 +1,13 @@
+package ru.tyanmt.task;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
